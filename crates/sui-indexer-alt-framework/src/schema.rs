dc356b00502ef26e787c61502ef85bf550f4668e
Will Yang <willyang@mystenlabs.com>
2024-12-12 13:14:22 -0800

    rename cp to cp_sequence_number

--- crates/sui-indexer-alt-framework/src/schema.rs
+++ crates/sui-indexer-alt-framework/src/schema.rs
@@ -23,4 +23,7 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(cp_mapping, watermarks,);
+diesel::allow_tables_to_appear_in_same_query!(
+    cp_mapping,
+    watermarks,
+);
